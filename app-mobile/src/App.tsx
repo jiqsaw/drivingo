3b4a798060097b4340b38d244da256df66f13a15
Jiqsaw <burakucakan@gmail.com>
2024-07-15 16:49:49 +0100

    feat: introduce tabs

--- app-mobile/src/App.tsx
+++ app-mobile/src/App.tsx
@@ -1,4 +1,4 @@
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -23,22 +23,37 @@ import '@ionic/react/css/text-transformation.css';
 import TrafficSigns from './pages/TrafficSigns';
 import './theme/variables.css';
 
+import { library, playCircle, radio } from 'ionicons/icons';
+
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/traffic-signs">
-          <TrafficSigns />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Redirect exact path="/" to="/home" />
+          <Route path="/home" render={() => <Home />} exact={true} />
+          <Route path="/traffic-signs" render={() => <TrafficSigns />} exact={true} />
+        </IonRouterOutlet>
+
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={playCircle} />
+            <IonLabel>Theory Test</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="radio" href="/highway-code">
+            <IonIcon icon={radio} />
+            <IonLabel>Highway code</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="library" href="/traffic-signs">
+            <IonIcon icon={library} />
+            <IonLabel>Traffic signs</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
---
+++ feat-traffic-signs/src/index.tsx
@@ -0,0 +1,6 @@
+
+export const FeatTrafficSigns = () => {
+    return (
+        <p>heyo</p>
+    );
+};
