86053c84913c82c663d9c48329e2546bdfd7431d
yasin özcan <yasino@outlook.com>
2024-07-18 18:43:05 +0300

    fix:roolback navigation

--- app-mobile/src/pages/Home.tsx
+++ app-mobile/src/pages/Home.tsx
@@ -3,34 +3,33 @@ import { UIButton, UILink } from '@drivingo/ui/components';
 import {
   IonContent,
   IonHeader,
-  IonNav,
   IonPage,
   IonRouterLink,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
 
-
-import CarLanding from './car/Home';
-
 const Home: React.FC = () => {
   return (
     <IonPage>
-      {/* <IonHeader>
+      <IonHeader>
         <IonToolbar>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
-      </IonHeader> */}
+      </IonHeader>
       <IonContent fullscreen className='ion-padding'>
-        {/* <IonRouterLink routerDirection='root' routerLink='/traffic-signs'>
+        <IonRouterLink routerDirection='root' routerLink='/traffic-signs'>
           <p>Traffic Signs</p>
         </IonRouterLink>
+
+        <IonRouterLink routerDirection='forward' routerLink='/settings'>
+          <p>Settings</p>
+        </IonRouterLink>
+
         <UIButton text='example button' /><br /><br />
         <span> example data: {DATA_TOPICS[0].name} </span>
         <br /><br />
-        <UILink text='link example' /> */}
-
-        <IonNav root={() => <CarLanding />}></IonNav>
+        <UILink text='link example' />
 
 
       </IonContent>
