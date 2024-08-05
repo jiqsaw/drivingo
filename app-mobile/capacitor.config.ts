1400b9dda4865e8067c2598b207c45a0bffb8926
Jiqsaw <burakucakan@gmail.com>
2024-08-05 13:45:53 +0300

    feat: set gcloud deploy for the dev env

--- app-mobile/capacitor.config.ts
+++ app-mobile/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'mobile',
-  webDir: '../dist/app-mobile',
+  webDir: 'build',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
