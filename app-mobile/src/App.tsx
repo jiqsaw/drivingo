c80769f04ac1cc93c2bfcfb7e8c829404f6502bf
yasin özcan <yasino@outlook.com>
2024-08-21 20:30:31 +0300

    feat:Network connection warning

--- app-mobile/src/App.tsx
+++ app-mobile/src/App.tsx
@@ -1,22 +1,55 @@
 import { IonApp, setupIonicReact } from '@ionic/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { storeUiSelectors } from 'store/src/ui/ui';
+import { Network } from '@capacitor/network';
+import { UINetworkWarning } from '@drivingo/ui/components';
+
 
 import AppRouter from './AppRouter';
 
 setupIonicReact();
 
+export const logCurrentNetworkStatus = async () => {
+    const status = await Network.getStatus();
+    return status;
+};
+
 const App: React.FC = () => {
 
     const theme = useSelector(storeUiSelectors.selectUiTheme);
+    const [networkControl, setNetworkControl] = useState(
+        {
+            connected: true,
+            connectionType: ''
+        }
+    );
 
     useEffect(() => {
         document.documentElement.classList.toggle('ion-palette-dark', theme === 'dark');
     }, [theme]);
 
+    useEffect(() => {
+        Network.addListener('networkStatusChange', status => {
+            setNetworkControl(status);
+        });
+
+        logCurrentNetworkStatus().then((response) => {
+            setNetworkControl(response);
+        });
+
+        return () => {
+            Network.removeAllListeners();
+        };
+    }, [])
+
+
+
     return (
         <IonApp>
+            {
+                !networkControl.connected && <UINetworkWarning />
+            }
             <AppRouter />
         </IonApp>
     );
--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button';
 export * from './link';
 export * from './modal/inline-modal';
+export * from './network-warning/network-warning';
 
