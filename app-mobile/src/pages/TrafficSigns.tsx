29a57eccd8c644d894e0e86199be50b0cbafe0b5
Yasino <yasino@outlook.com>
2024-10-26 19:17:52 +0300

    feat:added scroll up to traffic signs

--- app-mobile/src/pages/TrafficSigns.tsx
+++ app-mobile/src/pages/TrafficSigns.tsx
@@ -1,18 +1,37 @@
 import { FeatTrafficSigns } from '@drivingo/feat-traffic-signs';
-import { IonContent, IonPage } from '@ionic/react';
+import { UIScrollUp } from '@drivingo/ui';
+import { IonContent, IonPage, ScrollDetail } from '@ionic/react';
+import { createRef, useState } from 'react';
 import BottomNavigation from '../components/bottom-navigation/bottom-navigation';
 import Header from '../components/headers/header/header';
 
 const TrafficSigns: React.FC = () => {
+    const contentRef = createRef<HTMLIonContentElement>();
+    const [visibledScrollTop, setVisibledScrollTop] = useState(false);
+
     return (
         <IonPage>
-            <IonContent fullscreen>
+            <IonContent
+                fullscreen
+                className="main-content"
+                scrollEvents={true}
+                ref={contentRef}
+                onIonScroll={(ev: CustomEvent<ScrollDetail>) => {
+                    setVisibledScrollTop(ev.detail.scrollTop > 300);
+                }}
+            >
                 <aside className="container">
                     <Header />
                     <FeatTrafficSigns />
                 </aside>
                 <BottomNavigation />
             </IonContent>
+            <UIScrollUp
+                visibled={visibledScrollTop}
+                handleScrollUp={() => {
+                    contentRef.current?.scrollToTop(0);
+                }}
+            />
         </IonPage>
     );
 };
