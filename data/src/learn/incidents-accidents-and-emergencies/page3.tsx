5f539a6745f5813d4dc887ffd593848f47ed6360
BobHasanKokes <hasankokes@protonmail.com>
2024-12-02 10:18:24 +0300

    incidents and emergencies page3 details changed

--- data/src/learn/incidents-accidents-and-emergencies/page3.tsx
+++ data/src/learn/incidents-accidents-and-emergencies/page3.tsx
@@ -1,15 +1,12 @@
-const title = `Insurance`;
+const title = `Warning others of an incident`;
 const content = `
-You must have at least third-party insurance before driving on public roads. This insurance covers:
-•Injuries to other people.
-•Damage to others' property.
-•Damage to other vehicles.
-Driving without insurance is illegal and can result in an unlimited fine or disqualification. Provide your insurance certificate when taxing your vehicle or if requested by a police officer. You may initially receive a temporary cover note until the insurance certificate is issued.
-Before driving another person’s vehicle, confirm that it is insured for your use. Insurance costs depend on various factors but are generally lower if you:
-•Are over 25 years old.
-•Complete the Pass Plus scheme (not available in Northern Ireland).
-Insurance policies may include an excess, e.g., £100, meaning you'll pay the first £100 of any damage claim. The Pass Plus scheme can help newly qualified drivers improve skills and potentially reduce insurance costs.
+Use hazard warning lights:
 
+When slowing down or stopping suddenly on a motorway or unrestricted dual carriageway due to an incident or hazard ahead.
+
+When broken down or temporarily obstructing traffic.
+
+Place a warning triangle at least 45 metres (147 feet) behind your vehicle to alert other road users of a breakdown. Never use a warning triangle on a motorway.
 `;
 
 export default {
